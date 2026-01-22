c2e76a62807a5eb0d05365a3d573799d92de7ae5
Ramón Camacho Rojas <wenreidesign@gmail.com>
2026-01-22 15:05:27 +0000

    Generador de excusa terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let randWho = Math.floor(Math.random()*who.length);
+  let randAction = Math.floor(Math.random()*action.length);
+  let randWhat = Math.floor(Math.random()*what.length);
+  let randwhen = Math.floor(Math.random()*when.length);
+  
+  let newexcuse = who[randWho] + ' ' + action[randAction] + ' ' + what[randWhat] + ' ' + when[randwhen];
+  console.log (newexcuse)
+
+  let bariable = document.getElementById('excuse');
+  let button = document.getElementById("generate");
+
+  bariable.innerHTML = newexcuse
+
+  button.addEventListener("click", () => {
+    bariable.innerHTML = newexcuse();
+  });
 };
